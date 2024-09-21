3315ffe6e3bff573101b570bb5944de37ccbf2ac
MasubaCoder10 <masubacoder10@gmail.com>
2024-09-21 14:11:59 +0200

    added the pop up menu

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -1,11 +1,9 @@
 "use client";
 
 import { useState } from 'react';
-import { Dialog, DialogPanel } from '@headlessui/react'
-import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
-// import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
+import { Dialog, DialogPanel } from '@headlessui/react';
+import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline';
 import Image from 'next/image';
-import Script from 'next/script';
 import style from './navBar.module.css';
 
 const navigation = [
@@ -13,7 +11,7 @@ const navigation = [
   { name: 'Programmes', href: '#programmes' },
   { name: 'Blog', href: '#blog' },
   { name: 'A propos', href: '#aboutus' },
-]
+];
 
 const NavBar = () => {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
@@ -22,7 +20,7 @@ const NavBar = () => {
   const handleLanguageChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
     const newLang = event.target.value;
     setSelectedLang(newLang);
-  }
+  };
 
   const getFlagSrc = (lang: string): string => {
     switch (lang) {
@@ -31,28 +29,25 @@ const NavBar = () => {
       case 'fr':
         return '/flags/fr.png';
       default:
-        return '/flags/en/png';
+        return '/flags/en.png';
     }
-  }
-
+  };
 
   return (
-
-
-    <header id="home" className="absolute inset-x-0  top-0 z-50 ">
-      <nav aria-label="Global" className="flex items-center justify-between p-6 lg:px-8 fixed top-0 w-full bg-white bg-opacity-90">
-        <div className="flex lg:flex-1">
+    <header id="home" className="absolute inset-x-0 top-0 z-50">
+      <nav aria-label="Global" className=" flex items-center justify-around p-6 lg:px-8 fixed top-0 w-full bg-white bg-opacity-90">
+        <div className="flex lg:flex">
           <a href="#home" className="-m-1.5 p-1.5 flex items-center gap-2">
             <span className="sr-only">Next Leadership</span>
             <Image src="/logo.svg" alt="logo" className="h-10 w-auto" width={100} height={100} />
             <span className="text-2xl font-bold text-indigo-600">Next Leadership</span>
           </a>
           <div className='md:hidden flex items-center sm:ml-20'>
-          <select
+            <select
               value={selectedLang}
               onChange={handleLanguageChange}
               className={`appearance-none border-none outline-none bg-transparent text-sm ${style.navbar_language}`}
-              style={{ backgroundImage: `url(${getFlagSrc(selectedLang)})`, backgroundSize: '24px 16px', backgroundRepeat: 'no-repeat', backgroundPosition: 'center left 8px' }}
+              style={{ backgroundImage: `url(${getFlagSrc(selectedLang)})`, backgroundSize: '24px 20px', backgroundRepeat: 'no-repeat', backgroundPosition: 'center left 8px' }}
             >
               <option value="en" className={`${style.option_with_flag}`}>EN</option>
               <option value="fr" className={`${style.option_with_flag}`}>FR</option>
@@ -75,87 +70,69 @@ const NavBar = () => {
               {item.name}
             </a>
           ))}
-          <div className="relative flex items-center">
+          
+        </div>
+        <div className="hidden relative md:flex items-center">
             <select
               value={selectedLang}
               onChange={handleLanguageChange}
-              className={`appearance-none border-none outline-none bg-transparent text-sm ${style.navbar_language}`}
+              className={`appearance-none border-none outline-none bg-transparent shadow-lg text-sm py-2 px-4 ${style.navbar_language}`}
               style={{ backgroundImage: `url(${getFlagSrc(selectedLang)})`, backgroundSize: '24px 16px', backgroundRepeat: 'no-repeat', backgroundPosition: 'center left 8px' }}
             >
               <option value="en" className={`${style.option_with_flag}`}>EN</option>
               <option value="fr" className={`${style.option_with_flag}`}>FR</option>
             </select>
           </div>
-        </div>
-
-        <div className="hidden md:flex lg:flex-1 md:justify-end">
-          {/* <SignedOut>
-              <a href="/sign-in" className="text-sm font-semibold leading-6 text-gray-900">
-                Log in <span aria-hidden="true">&rarr;</span>
-              </a>
-            </SignedOut>
-            <SignedIn>
-              <UserButton showName={true} />
-              
-            </SignedIn> */}
-        </div>
       </nav>
+
       <Dialog open={mobileMenuOpen} onClose={setMobileMenuOpen} className="lg:hidden">
-        
-        <div className="fixed inset-0 z-50" />
-        <DialogPanel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
-        
-          <div className="flex items-center justify-between">
-            
-            <a href="#home" className="-m-1.5 p-1.5" onClick={() => setMobileMenuOpen(false)}>
-              <span className="sr-only">Next</span>
-              <Image src="/logo.svg" alt="logo" className="h-10 w-auto" width={100} height={100} />
+        <div className="fixed inset-0 z-50 bg-black bg-opacity-50" />
+        <DialogPanel className="fixed inset-0 z-50 flex items-center justify-center px-4">
+          <div className="relative bg-white rounded-lg p-6 w-full max-w-sm">
+            <div className="flex items-center justify-between">
+              <a href="#home" className="-m-1.5 p-1.5" onClick={() => setMobileMenuOpen(false)}>
+                <span className="sr-only">Next</span>
+                <Image src="/logo.svg" alt="logo" className="h-10 w-auto" width={100} height={100} />
+              </a>
+              <button
+                type="button"
+                onClick={() => setMobileMenuOpen(false)}
+                className="-m-2.5 rounded-md p-2.5 text-gray-700"
+              >
+                <span className="sr-only">Close menu</span>
+                <XMarkIcon aria-hidden="true" className="h-6 w-6" />
+              </button>
+            </div>
 
-            </a>
-
-           
-
-            <button
-              type="button"
-              onClick={() => setMobileMenuOpen(false)}
-              className="-m-2.5 rounded-md p-2.5 text-gray-700"
-            >
-              <span className="sr-only">Close menu</span>
-              <XMarkIcon aria-hidden="true" className="h-6 w-6" />
-            </button>
-           
-          </div>
-          <div className="mt-6 flow-root">
-            <div className="-my-6 divide-y divide-gray-500/10">
-              <div className="space-y-2 py-6">
-                {navigation.map((item) => (
+            <div className="mt-6 flow-root">
+              <div className="-my-6 divide-y divide-gray-500/10">
+                <div className="space-y-2 py-6">
+                  {navigation.map((item) => (
+                    <a
+                      key={item.name}
+                      href={item.href}
+                      className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                      onClick={() => setMobileMenuOpen(false)}
+                    >
+                      {item.name}
+                    </a>
+                  ))}
+                </div>
+                <div className="py-6">
                   <a
-                    key={item.name}
-                    href={item.href}
-                    className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                    href="/sign-in"
+                    className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
                     onClick={() => setMobileMenuOpen(false)}
                   >
-                    {item.name}
+                    Log in
                   </a>
-                ))}
-                
-              </div>
-              <div className="py-6">
-                <a
-                  href="/sign-in"
-                  className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-                  onClick={() => setMobileMenuOpen(false)}
-                >
-                  Log in
-                </a>
+                </div>
               </div>
             </div>
           </div>
         </DialogPanel>
       </Dialog>
-
     </header>
-
   );
 };
 
