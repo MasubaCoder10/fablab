d82333717ede2890a2f0a438f9f6d315b2939aca
MasubaCoder10 <masubacoder10@gmail.com>
2024-10-28 14:28:55 +0100

    fixex the ongoingproject error

--- src/app/[locale]/data/ongoingProjects.ts
+++ src/app/[locale]/data/ongoingProjects.ts
@@ -1,6 +1,6 @@
-import imgGec from '../../../assets/images/GEC.jpg'; 
-import imgTuring from '../../../assets/images/Turing.webp'; 
-import imgCreativity from '../../../assets/images/Cretivity.png'; 
+import imgGec from '../../../assets/images/GEC.jpg';
+import imgTuring from '../../../assets/images/Turing.webp';
+import imgCreativity from '../../../assets/images/Cretivity.png';
 import { useTranslations } from 'next-intl';
 
 
@@ -9,9 +9,9 @@ export function OngoingProjects() {
 
   return [
     {
-        id:100,
-        title: t('GEC_ZARMA_100.title'),
-        date: t('GEC_ZARMA_100.date'),
+      id: 100,
+      title: t('GEC_ZARMA_100.title'),
+      date: t('GEC_ZARMA_100.date'),
       authors: [
         "MamadouK.KEITA",
         "Christopher Homan",
@@ -35,59 +35,59 @@ export function OngoingProjects() {
       imageUrl: imgGec,
     },
     {
-      id:101,
+      id: 101,
       title: t('Turing_101.title'),
       date: t('Turing_101.date'),
-    authors: [
-      "MamadouK.KEITA",
-      "Christopher Homan",
-      "Abdoulaye Hamani Sofianou",
-      "Adwoa Bremang",
-      "Marcos Zampieri",
-      "Habibatou Abdoulaye Alfari",
-      "Elysabhete Amadou Ibrahim",
-      "Dennis Owusu"
-    ],
-    publisher: [
-      "NILab",
-      "Ashesi University",
-      "Rochester Institute of Technology",
-      "George Mason University"
-    ],
-    researchTopics: [
-      "NLP, low research language, Error correction"
-    ],
-    description: t('Turing_101.description'),
-    imageUrl: imgTuring,
-  },
-   
-  {
-    id:102,
-    title: t('ASR_102.title'),
-    date: t('ASR_102.date'),
-  authors: [
-    "MamadouK.KEITA",
-    "Christopher Homan",
-    "Abdoulaye Hamani Sofianou",
-    "Adwoa Bremang",
-    "Marcos Zampieri",
-    "Habibatou Abdoulaye Alfari",
-    "Elysabhete Amadou Ibrahim",
-    "Dennis Owusu"
-  ],
-  publisher: [
-    "NILab",
-    "Ashesi University",
-    "Rochester Institute of Technology",
-    "George Mason University"
-  ],
-  researchTopics: [
-    "NLP, low research language, Error correction"
-  ],
-  description: t('ASR_102.description'),
-  imageUrl: imgCreativity,
-},
-];
+      authors: [
+        "MamadouK.KEITA",
+        "Christopher Homan",
+        "Abdoulaye Hamani Sofianou",
+        "Adwoa Bremang",
+        "Marcos Zampieri",
+        "Habibatou Abdoulaye Alfari",
+        "Elysabhete Amadou Ibrahim",
+        "Dennis Owusu"
+      ],
+      publisher: [
+        "NILab",
+        "Ashesi University",
+        "Rochester Institute of Technology",
+        "George Mason University"
+      ],
+      researchTopics: [
+        "NLP, low research language, Error correction"
+      ],
+      description: t('Turing_101.description'),
+      imageUrl: imgTuring,
+    },
+
+    {
+      id: 102,
+      title: t('ASR_102.title'),
+      date: t('ASR_102.date'),
+      authors: [
+        "MamadouK.KEITA",
+        "Christopher Homan",
+        "Abdoulaye Hamani Sofianou",
+        "Adwoa Bremang",
+        "Marcos Zampieri",
+        "Habibatou Abdoulaye Alfari",
+        "Elysabhete Amadou Ibrahim",
+        "Dennis Owusu"
+      ],
+      publisher: [
+        "NILab",
+        "Ashesi University",
+        "Rochester Institute of Technology",
+        "George Mason University"
+      ],
+      researchTopics: [
+        "NLP, low research language, Error correction"
+      ],
+      description: t('ASR_102.description'),
+      imageUrl: imgCreativity,
+    },
+  ];
 }
 
 
